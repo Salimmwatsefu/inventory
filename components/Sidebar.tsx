9a44725023b52cf0612cd7bc003a617665e20b02
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2023-07-21 18:55:16 +0300

    logout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -245,30 +245,6 @@ export default function Sidebar() {
 
           <span className="text-sm font-medium"> Sign Up</span>
         </button>
-
-        <form action="/logout">
-          <button
-            type="submit"
-            className="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-white hover:bg-gray-100 hover:text-gray-700"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 opacity-75"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-              />
-            </svg>
-
-            <span className="text-sm font-medium"> Logout </span>
-          </button>
-        </form>
       </nav>
     </details>
   </nav>
